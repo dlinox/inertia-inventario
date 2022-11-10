8adbce013229e35c0913b3e41fe615dbf48515d0
Denis Puma <dpumaticona@gmail.com>
2022-11-10 18:30:04 -0500

    tabla de prueba

---
+++ database/migrations/2022_11_10_173842_create_iventario_olds_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIventarioOldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('iventario_olds', function (Blueprint $table) {
+            $table->id();
+            $table->char('codigo', 12)->unique();
+            $table->char('codigo_siga', 12);
+            $table->string('descripcion', 100)->nullable();
+            $table->string('modelo', 50)->nullable();
+            $table->string('marca', 50)->nullable();
+            $table->string('nro_serie', 30)->nullable();
+            $table->char('anio_fabricacion')->nullable(); //cambiar a date
+            $table->string('estado_actual', 20);
+            $table->string('ubicación', 120);
+            $table->char('persona_dni', 8);
+            $table->integer('nro_cargo_personal');
+            $table->date('fecha_cargo');
+            $table->integer('nro_orden')->nullable();
+            $table->date('fecha_compra')->nullable();
+            $table->char('proveedor_ruc', 11)->nullable();
+            $table->integer('nro_pecosa');
+            $table->date('fecha_pecosa')->nullable();
+            $table->double('vida_util', 7, 6)->nullable();
+            $table->date('fecha_vida_util')->nullable();
+            $table->double('valor_adquisicion', 10, 6)->default(0);
+            $table->double('valor_inicial', 10, 2);
+            $table->double('valor_depreciacion', 10, 2);
+            $table->date('fecha_baja_bien')->nullable();
+            $table->string('clasificador', 15);
+            $table->char('sub_cta', 10);
+            $table->integer('mayor');
+            $table->string('observaciones', 150);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('iventario_olds');
+    }
+}
