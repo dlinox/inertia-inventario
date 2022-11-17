842faf37f9abe2d98befc28eab3944362a251fd3
Denis Puma <dpumaticona@gmail.com>
2022-11-17 09:29:19 -0500

    Actualizacion tabla Inventario

--- app/Models/Inventario.php
+++ app/Models/Inventario.php
@@ -10,21 +10,75 @@ class Inventario extends Model
     use HasFactory;
 
     protected $table = 'inventario';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     protected $fillable = [
         'codigo',
-        'codigo_anterior',
-        'nombre',
+        'codigo_siga',
+        'descripcion',
         'modelo',
-        'numero',
-        'serie',
+        'marca',
+        'nro_serie',
+        'anio_fabricacion',
+        'nro_cargo_personal',
+        'fecha_cargo',
+        'nro_orden',
+        'fecha_compra',
+        'proveedor_ruc',
+        'nro_pecosa',
+        'fecha_pecosa',
+        'vida_util',
+        'fecha_vida_util',
+        'valor_adquisicion',
+        'valor_inicial',
+        'valor_depreciacion',
+        'fecha_baja_bien',
+        'clasificador',
+        'sub_cta',
+        'mayor',
+        'observaciones',
+        'tipo',
         'idbienk',
-        'id_persona',
+        'estado',
         'idpersona_otro',
+        'id_persona',
         'id_area',
         'id_usuario',
         'id_estado',
-        'estado',
-        'observaciones',
     ];
 
     public function getDataByCode($codigo)
