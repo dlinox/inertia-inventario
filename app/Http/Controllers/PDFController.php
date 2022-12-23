e09a9415387f622ddfd374c5e59dbb755f570ef4
Lino Puma Ticona <nearlino20@gmail.com>
2022-12-23 14:59:17 -0500

    dasdad

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -155,7 +155,7 @@ class PDFController extends Controller
         else { $responsable2 = null; }
 
         $inventaristas = DB::select('SELECT * FROM users WHERE ID IN ( SELECT ID_USUARIO from inventario WHERE id_area = "' . $idArea . '" and id_persona = ' . $idP . ');');
-        $bienes = DB::select('SELECT * from inventario WHERE  id_area = "'.$idArea.'" AND id_persona = ' . $idP . ';');
+        $bienes = DB::select('SELECT * from inventario limit 5000');// WHERE  id_area = "'.$idArea.'" AND id_persona = ' . $idP . ';');
         $ldate = date('Y-m-d');
         $lhour = date('H:i:s');
 
