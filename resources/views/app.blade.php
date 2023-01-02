f873b48dbc86cd4b29f2c9fac5f9eec9b7a57605
Ariel Luque Cusacani <55467199+ariel-web@users.noreply.github.com>
2023-01-02 04:22:38 -0500

    x

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,14 +5,28 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <script>
-        function lock(orientation){
-            let de = document.documentElement;
-            if(de.requestFullScreen){de.requestFullScreen();}
-            else if(de..mozRequestFullScreen){de.mozRequestFullScreen();}
-            else if(de..webkitRequestFullscreen){de.webkitRequestFullscreen();}
-            else if(de.msRequestFullscreen){de.msRequestFullscreen();}
-            
-            screen.orientation.lock(orientation)
+        function lock (orientation) {
+        // (A1) GO INTO FULL SCREEN FIRST
+        let de = document.documentElement;
+        if (de.requestFullscreen) { de.requestFullscreen(); }
+        else if (de.mozRequestFullScreen) { de.mozRequestFullScreen(); }
+        else if (de.webkitRequestFullscreen) { de.webkitRequestFullscreen(); }
+        else if (de.msRequestFullscreen) { de.msRequestFullscreen(); }
+
+        // (A2) THEN LOCK ORIENTATION
+        screen.orientation.lock(orientation);
+        }
+
+        // (B) UNLOCK SCREEN ORIENTATION
+        function unlock () {
+        // (B1) UNLOCK FIRST
+        screen.orientation.unlock();
+
+        // (B2) THEN EXIT FULL SCREEN
+        if (document.exitFullscreen) { document.exitFullscreen(); }
+        else if (document.webkitExitFullscreen) { document.webkitExitFullscreen(); }
+        else if (document.mozCancelFullScreen) { document.mozCancelFullScreen(); }
+        else if (document.msExitFullscreen) { document.msExitFullscreen(); }
         }
     </script>
     <style>
@@ -53,10 +67,19 @@
 </head>
 
 <body>
-    <div style="margin:39px">
-        <input type="button" value="lock" onclick="lock('portrait')"> 
-    </div>
-    @inertia
+        <h1>LOCK PORTRAIT</h1>
+        <input type="button" value="Portrait" onclick="lock('portrait')"/>
+        <input type="button" value="Portrait Primary" onclick="lock('portrait-primary')"/>
+        <input type="button" value="Portrait Secondary" onclick="lock('portrait-secondary')"/>
+        
+        <h1>LOCK LANDSCAPE</h1>
+        <input type="button" value="Landscape" onclick="lock('landscape')"/>
+        <input type="button" value="Landscape Primary" onclick="lock('landscape-primary')"/>
+        <input type="button" value="Landscape Secondary" onclick="lock('landscape-secondary')"/>
+        
+        <h1>UNLOCK</h1>
+        <input type="button" value="Unlock" onclick="unlock()"/>
+    {{-- @inertia --}}
 </body>
 
 </html>
