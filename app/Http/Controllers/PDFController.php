7979b8d1d5a30f3d697ee3d484bdab1e04235364
Ariel Luque Cusacani <55467199+ariel-web@users.noreply.github.com>
2023-01-06 06:26:24 -0500

    sd

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -120,10 +120,10 @@ class PDFController extends Controller
             $pdf->setOptions([
                 'header-html'=>view('cargos._headercargo',compact('oficina','responsable','responsable2','num_doc')),
                 'footer-html'=>view('cargos._footercargo'),  
-                'margin-bottom'=>'4cm',
+                'margin-bottom'=>'4.1cm',
                 'margin-left'=>'0.5cm',
                 'margin-right'=>'0.5cm',
-                'margin-top'=>'3.5cm',
+                'margin-top'=>'3.6cm',
                 'encoding'=>'UTF-8',
                 'orientation'=>'landscape',
                 'page-size'=>'a4'
